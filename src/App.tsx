3cc87108c588bef1e6b57cfc063be61bdf019025
Ryan Park <dongwhee@gmail.com>
2021-01-03 11:57:40 +0900

    add qr reader module, setup project settings (eslint and more)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
-import React from 'react';
-import './App.css';
-import { Button, Card, Container, Navbar } from 'react-bootstrap';
+import React, { useState } from 'react'
+import './App.css'
+import { Button, Card, Container, Navbar } from 'react-bootstrap'
+import QrReader from 'react-qr-reader'
 
 export const App: React.FC = () => {
+  const [result, setResult] = useState('')
+  const [facing, setFacing] = useState<'environment' | 'user' | undefined>(
+    'environment'
+  )
+  const handleScan = (data: string | null) => {
+    if (data) {
+      setResult(data)
+    }
+  }
+  const handleError = (err: unknown) => {
+    console.log(err)
+  }
+  const changeCamera = () => {
+    if (facing === 'environment') {
+      setFacing('user')
+    } else {
+      setFacing('environment')
+    }
+  }
   return (
     <>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand>Notto</Navbar.Brand>
-    </Navbar>
-    <Container className="p-3">
-        <Card className="text-center">
-      <Card.Header>QR Code</Card.Header>
-          <Card.Body>Hello</Card.Body>
+      <Navbar bg='primary' expand='lg'>
+        <Navbar.Brand className='text-light'>Notto</Navbar.Brand>
+      </Navbar>
+      <Container className='p-3'>
+        <Card className='text-center'>
+          <Card.Header>QR Code</Card.Header>
+          <Card.Body>
+            <QrReader
+              delay={500}
+              onScan={handleScan}
+              onError={handleError}
+              facingMode={facing}
+            />
+          </Card.Body>
           <Card.Footer>
-      <Button variant="primary">Button</Button>
+            <Button variant='primary' onClick={changeCamera}>
+              Change Camera
+            </Button>
           </Card.Footer>
-      </Card>
-    </Container>
+        </Card>
+      </Container>
     </>
-  );
+  )
 }
